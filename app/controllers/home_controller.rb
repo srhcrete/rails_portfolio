22f57e1a4b986e7c0d2b8d413d60eabff584569a
Sarah Lindmar <srhcrete@gmail.com>
2018-01-01 15:44:13 -0800

    add posts view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @projects = Project.all
     @skills = Skill.all
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   resources :projects
   resources :skills
+  resources :posts
 end
